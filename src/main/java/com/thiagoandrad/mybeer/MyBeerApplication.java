cb72d88c779b9eaccf50fbebeb14433dbb6af552
ThiagoAndrad <thpandrade@gmail.com>
2018-06-17 00:59:29 -0300

    initial commit - configuring depedencies

---
+++ src/main/java/com/thiagoandrad/mybeer/MyBeerApplication.java
@@ -0,0 +1,12 @@
+package com.thiagoandrad.mybeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBeerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBeerApplication.class, args);
+    }
+}
